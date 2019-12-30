a6c37df22e03e36fdd5051456ede8301430f837e
Gabriel Gordon <gabegor94@gmail.com>
2019-12-30 17:42:43 +0200

    finished adding curse function and jokes and others

--- boto.py
+++ boto.py
@@ -3,23 +3,74 @@ This is the template server side for ChatBot
 """
 from bottle import route, run, template, static_file, request
 import json
-
+import random
+counter=0
 
 def getAnswer(input):
-    if input.endswith('?'):
-        return handleInput(input,'question')
-    elif input in ['hello','hi','hey'] :
-        return handleInput(input,'greeting')
+    global counter
+    counter+=1
+    response = {'msg': input, 'animation': ''}
+    if checkForSwearWord(input):
+        response['msg'] = handleInput(input,'curse')
+        response['animation'] = 'no'
+    elif 'joke' in input.lower():
+        response['msg'] = handleInput(input, 'joke')
+        response['animation'] = 'giggling'
+    elif input.endswith('?'):
+        response['msg'] = handleInput(input, 'question')
+        response['animation'] = 'dancing'
+    elif input in ['hello', 'hi', 'hey']:
+        response['msg'] = handleInput(input, 'greeting')
+        response['animation'] = 'excited'
     elif input.endswith('!'):
-        return "Woh! You're excited to meet me"
-    return 'default'
+        response['msg'] = "Woh! You're excited to meet me"
+        response['animation'] = 'afraid'
+    elif input.startswith("I") or input.startswith('i'):
+        response['msg'] = "Stop talking about yourself."
+        response['animation'] = 'bored'
+    elif input.lower() == 'count':
+        response['msg'] = f"You have asked me {counter} questions"
+        response['animation'] = 'takeoff'
+    elif 'love' in input.lower():
+        response['msg'] = handleInput(input,'love')
+        response['animation'] = 'inlove'
+    elif 'dog' in input.lower():
+        response['msg'] = handleInput(input, 'dog')
+        response['animation'] = 'dog'
+    else:
+        response['msg'] = "I don't understand"
+        response['animation'] = 'confused'
+    return response
 
-def handleInput(input,type):
-    if type=='question':
+
+def handleInput(input, type):
+    if type == 'question':
         return getAnswerToQuestion(input)
     elif type == 'greeting':
         return getAnswerToGreeting(input)
+    elif type == 'love':
+        return getAnswerToLoveQuestion(input)
+    elif type == 'curse':
+        return getAnswerToCurse(input)
+    elif type == 'dog':
+        return getAnswerToDog(input)
+    elif type == 'joke':
+        return getJoke(input)
     return 'default'
+
+def getJoke(input):
+    jokesList=['I ate a clock yesterday, it was very time-consuming', 'Have you played the updated kids’ game? I Spy With My Little Eye . . . Phone.','A perfectionist walked into a bar…apparently, the bar wasn’t set high enough.','Did you hear about the semi-colon that broke the law? He was given two consecutive sentences.','What’s the difference between ignorance and apathy? I don’t know and I don’t care.']
+    num=random.randint(0,len(jokesList)-1)
+    return jokesList[num]
+
+def getAnswerToDog(input):
+    return 'I love dogs!'
+
+def getAnswerToCurse(input):
+    return "Don't use that type of language! "
+
+def getAnswerToLoveQuestion(input):
+    return "Love you too!"
 
 def getAnswerToQuestion(input):
     return 'this is a question'
@@ -27,15 +78,26 @@ def getAnswerToQuestion(input):
 def getAnswerToGreeting(input):
     return "Hello! How are you?"
 
+def checkForSwearWord(input):
+    list=input.split(' ')
+    badWords = ["4r5e", "5h1t", "5hit", "a55", "anal", "anus", "ar5e", "arrse", "arse", "ass", "ass-fucker", "asses", "assfucker", "assfukka", "asshole", "assholes", "asswhole", "a_s_s", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", "balls", "ballsack", "bastard", "beastial", "beastiality", "bellend", "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob", "blowjobs", "boiolas", "bollock", "bollok", "boner", "boob", "boobs", "booobs", "boooobs", "booooobs", "booooooobs", "breasts", "buceta", "bugger", "bum", "bunny fucker", "butt", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", "cockface", "cockhead", "cockmunch", "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", "cocksucking", "cocksucks", "cocksuka", "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", "cum", "cummer", "cumming", "cums", "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", "cuntlicker", "cuntlicking", "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", "cyberfuckers", "cyberfucking", "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", "dlck", "dog-fucker", "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", "ejaculates", "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", "fingerfuck", "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", "fistfucked", "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", "fooker", "fuck", "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", "fuckings", "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", "gangbang", "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", "goddamn", "goddamned", "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "horniest", "horny", "hotsex", "jack-off", "jackoff", "jap", "jerk-off", "jism", "jiz", "jizm", "jizz", "kawk", "knob", "knobead", "knobed", "knobend", "knobhead", "knobjocky", "knobjokey", "kock", "kondum", "kondums", "kum", "kummer", "kumming", "kums", "kunilingus", "l3i+ch", "l3itch", "labia", "lust", "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "masterb8", "masterbat*", "masterbat3", "masterbate", "masterbation", "masterbations", "masturbate", "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas", "mothafuckaz", "mothafucked", "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", "mothafucks", "mother fucker", "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", "motherfucking", "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", "muthafuckker", "muther", "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah", "niggas", "niggaz", "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", "nutsack", "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", "phonesex", "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", "piss", "pissed", "pisser", "pissers", "pisses", "pissflaps", "pissin", "pissing", "pissoff", "poop", "porn", "porno", "pornography", "pornos", "prick", "pricks", "pron", "pube", "pusse", "pussi", "pussies", "pussy", "pussys", "rectum", "retard", "rimjaw", "rimming", "s hit", "s.o.b.", "sadist", "schlong", "screwing", "scroat", "scrote", "scrotum", "semen", "sex", "sh!+", "sh!t", "sh1t", "shag", "shagger", "shaggin", "shagging", "shemale", "shi+", "shit", "shitdick", "shite", "shited", "shitey", "shitfuck", "shitfull", "shithead", "shiting", "shitings", "shits", "shitted", "shitter", "shitters", "shitting", "shittings", "shitty", "skank", "slut", "sluts", "smegma", "smut", "snatch", "son-of-a-bitch", "spac", "spunk", "s_h_i_t", "t1tt1e5", "t1tties", "teets", "teez", "testical", "testicle", "tit", "titfuck", "tits", "titt", "tittie5", "tittiefucker", "titties", "tittyfuck", "tittywank", "titwank", "tosser", "turd", "tw4t", "twat", "twathead", "twatty", "twunt", "twunter", "v14gra", "v1gra", "vagina", "viagra", "vulva", "w00se", "wang", "wank", "wanker", "wanky", "whoar", "whore", "willies", "willy", "xrated", "xxx"];
+    for li in list:
+        if li.lower() in badWords:
+            return True
+    else:
+        return False
+
 
 @route('/', method='GET')
 def index():
     return template("chatbot.html")
 
+
 @route("/chat", method='POST')
 def chat():
     user_message = request.POST.get('msg')
-    return ({"animation": "inlove", "msg":getAnswer(user_message)})
+    return (getAnswer(user_message))
+
 
 @route("/test", method='POST')
 def chat():
@@ -61,6 +123,6 @@ def images(filename):
 def main():
     run(host='localhost', port=7000)
 
+
 if __name__ == '__main__':
     main()
-
